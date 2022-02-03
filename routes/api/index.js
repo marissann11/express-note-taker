753a67ab0de33604f33d874ded3e7295d012979b
marissann11 <wilhite.marissa@gmail.com>
2022-02-02 23:02:21 -0500

    add heroku

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,15 +5,20 @@ const db = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
     res.json(db);
-})
+});
 
 router.post('/notes', (req, res) => {
     const {title, text} = req.body;
-    const id = uuid.v4();
+    let id = uuid.v4();
     const newNote = {title, text, id};
     db.push(newNote);
     fs.writeFileSync('db/db.json', JSON.stringify(db))
     res.json(db);
-})
+});
+
+// router.delete('./notes:id', (req, res) => {
+//     let id = req.params.id
+// })
+// filter through and make array of all notes whose id's don't match id to be deleted, new array pushed to db?? 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const routes = require("./routes")
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
+
 app.use(express.json());
 
 app.use(routes)

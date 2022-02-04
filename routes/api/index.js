2d092cfa58352c337ca319294b36fc65542e9b5f
marissann11 <wilhite.marissa@gmail.com>
2022-02-03 23:50:40 -0500

    added delete note

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,24 +1,27 @@
-const router = require('express').Router();
-const uuid = require('uuid');
-const fs = require('fs');
-const db = require('../../db/db.json');
+const router = require("express").Router();
+const uuid = require("uuid");
+const fs = require("fs");
+const db = require("../../db/db.json");
+const findById = require("../../lib/notes.js")
 
-router.get('/notes', (req, res) => {
-    res.json(db);
+router.get("/notes", (req, res) => {
+  res.json(db);
 });
 
-router.post('/notes', (req, res) => {
-    const {title, text} = req.body;
-    let id = uuid.v4();
-    const newNote = {title, text, id};
-    db.push(newNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db))
-    res.json(db);
+router.post("/notes", (req, res) => {
+  const { title, text } = req.body;
+  let id = uuid.v4();
+  const newNote = { title, text, id };
+  db.push(newNote);
+  fs.writeFileSync("db/db.json", JSON.stringify(db));
+  res.json(db);
 });
 
-// router.delete('./notes:id', (req, res) => {
-//     let id = req.params.id
-// })
-// filter through and make array of all notes whose id's don't match id to be deleted, new array pushed to db?? 
+router.delete('/notes/:id', (req, res) => {
+    const foundNote = findById(req.body.id, db)
+    db.splice(foundNote, 1);
+    fs.writeFileSync("db/db.json", JSON.stringify(db));
+    res.json(db);
+})
 
 module.exports = router;
